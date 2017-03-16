32968c1543ba166dc988e68592383e3ca223f76b
刘延瑞 <liuyanruidx@hotmail.com>
2017-03-16 17:49:31 +0800

    水饺店

---
+++ WS.Web/Areas/WeiXin/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WS.Web.Areas.WeiXin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: WeiXin/Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Introduce()
+        {
+            return View();
+        }
+
+        public ActionResult Specialty()
+        {
+            return View();
+        }
+    }
+}
